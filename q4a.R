049266bdffbde424923e93ffef783604d38bb876
jiangyt2007 <77658366+jiangyt2007@users.noreply.github.com>
2021-12-31 13:07:05 +0800

    Update q4a.R

--- q4a.R
+++ q4a.R
@@ -1,4 +1,5 @@
-file <- readxl('2016_data.xlsx')
+library(readxl)
+file <- read_xlsx('2016_data.xlsx')
 
 # empty df to store the returns. Same size as file
 returns <- data.frame(matrix(ncol = ncol(file), nrow = nrow(file)))
@@ -16,19 +17,24 @@ for (stock_idx in 2:ncol(file)) {
     }
 }
 
-# empty vector to store VaR
+# empty vectors to store VaR and CVaR
 var95 <- c()
-
+cvar95 <- c()
 returns2 <- returns[-1] # remove 1st row of NA values
 
 # 5% quantile for each stock
 for (stock_idx in 2 : ncol(file)) {
-    var = quantile(returns2[, stock_idx], probs = 0.05)
+    curr_stock_returns = returns2[, stock_idx]
+    var = quantile(curr_stock_returns, probs = 0.05)
+    worst5percent = curr_stock_returns[curr_stock_returns < var]
     var95[stock_idx - 1] = var
+    cvar95[stock_idx - 1] = mean(worst5percent)
 }
 
 # percentages
 p <- c(.15, .2, .2, .15, .1, .15, .05)
 
-result <- p * var95
-print(sum(result))
+result_var <- p * var95
+result_cvar <- p * cvar95
+print(paste("VaR95 = ", sum(result_var), sep = ""))
+print(paste("CVaR95 = ", sum(result_cvar), sep = ""))
